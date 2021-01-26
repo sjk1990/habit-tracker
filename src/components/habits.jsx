3d23b8d95bb4504f8e64c391a4e14f70c1fade64
SJKim <worldsjk1990@gmail.com>
2021-01-26 23:41:49 +0900

    create navbar

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,45 +2,16 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Running", count: 0 },
-      { id: 2, name: "Eatting", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits: habits });
-    //this.setState({ habits })와 동일
-  };
-
-  handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits: habits });
-  };
-
-  handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits: habits });
-  };
-
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
           />
         ))}
       </ul>
