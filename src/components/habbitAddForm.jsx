29d05f283b47b64fb95aa389cdcdfe1e4d24db75
SJKim <worldsjk1990@gmail.com>
2021-01-27 11:41:40 +0900

    refactor function component & use memo

--- src/components/habbitAddForm.jsx
+++ src/components/habbitAddForm.jsx
@@ -1,32 +1,30 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 
-class HabbitAddForm extends PureComponent {
-  //   원하는요소에 연결
-  inputRef = React.createRef();
-  formRef = React.createRef();
+const HabbitAddForm = memo((props) => {
+  const inputRef = React.createRef();
+  const formRef = React.createRef();
 
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
+    const name = inputRef.current.value;
     // const name = event.target.inputTag.value;
-    name && this.props.onAdd(name);
-    // this.inputRef.current.value = "";  <-- 동일한 기능 수행
-    this.formRef.current.reset();
+    name && props.onAdd(name);
+    // inputRef.current.value = "";  <-- 동일한 기능 수행
+    formRef.current.reset();
   };
-  render() {
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          //   name="inputTag" <-- name속성 이용해서 연결가능
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Habit"
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        //   name="inputTag" <-- name속성 이용해서 연결가능
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabbitAddForm;
